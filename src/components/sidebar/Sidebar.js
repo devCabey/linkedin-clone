13954c583f06c53899879da3332a9df25e582d41
CalebOkyere <agyakojo4321@gmail.com>
2022-12-08 01:44:34 +0000

    onHover underline error resolved

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -19,16 +19,21 @@ function Sidebar() {
     <div className="sidebar_container">
       <div className="sidebar_top_wrapper">
         <div className="sidebar_top">
-          <img
-            src="https://img.freepik.com/free-vector/ocean-sea-waves-with-white-foam-nature-landscape_107791-1563.jpg?w=2000"
-            alt="background"
-          />
-          <Avatar
-            className="sidebar_avatar"
-            src="https://media.istockphoto.com/id/1179588723/photo/close-up-portrait-of-his-he-nice-attractive-funny-confused-brunet-bearded-guy-wearing-pink.jpg?s=612x612&w=0&k=20&c=nhgnEZKvA1yxYhnSQ-hD4fHFQr7xGn2TwJnatCdssK4="
-            sx={{ width: "60px", height: "60px" }}
-          />
-          <h2>Caleb Okyere</h2>
+          <div className="sidebar_top_hover">
+            <img
+              src="https://img.freepik.com/free-vector/ocean-sea-waves-with-white-foam-nature-landscape_107791-1563.jpg?w=2000"
+              alt="background"
+            />
+            <span>
+              <Avatar
+                className="sidebar_avatar"
+                src="https://media.istockphoto.com/id/1179588723/photo/close-up-portrait-of-his-he-nice-attractive-funny-confused-brunet-bearded-guy-wearing-pink.jpg?s=612x612&w=0&k=20&c=nhgnEZKvA1yxYhnSQ-hD4fHFQr7xGn2TwJnatCdssK4="
+                sx={{ width: "60px", height: "60px" }}
+              />
+            </span>
+
+            <h2>Caleb Okyere</h2>
+          </div>
           <h4>
             Software Engineer(Fullstack)-Javascript ||Typescript || Python
           </h4>
