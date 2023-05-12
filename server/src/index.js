1a3a1775b32e4dfcb07717957be4d462b309a7f3
Caleb Okyere <agyakojo4321@gmail.com>
2023-05-12 13:06:55 +0000

    changes made to router

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import { connect } from './model/index.js';
+import router from './router/index.js';
 
 const app = express();
 
 app.use(cors());
 app.use(morgan('tiny'));
+app.use('/api', router);
 
 connect()
 	.then(() => {
--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,6 +1,15 @@
-export * from './auth.js';
-export * from './get.js';
-export * from './post.js';
-export * from './patch.js';
-export * from './put.js';
-export * from './delete.js';
+import { Router } from 'express';
+
+const router = Router();
+
+/** ALL GET REQUEST */
+
+/** ALL POST REQUEST */
+
+/** ALL PATCH REQUEST */
+
+/** ALL PUT REQUEST */
+
+/** ALL DELETE REQUEST */
+
+export default router;
