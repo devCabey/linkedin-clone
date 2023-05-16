ea95e6173d6ea28d344dc76fe385729276cb141b
Caleb Okyere <agyakojo4321@gmail.com>
2023-05-16 19:19:35 +0000

    delete post route done

--- server/src/controller/delete.js
+++ server/src/controller/delete.js
@@ -1,4 +1,5 @@
-import { PostModel } from '../model/index.js';
+import { getUserInfo } from '../helper/index.js';
+import { PostModel, UserModel } from '../model/index.js';
 
 /**
  * DELETE: http://localhost:8080/api/posts/:id
@@ -9,19 +10,30 @@ import { PostModel } from '../model/index.js';
  */
 export const deletePost = async (req, res) => {
 	const { id } = req.params;
-	const token = req.headers.authorization;
 	try {
 		if (!id) throw new Error('Post ID not provided');
-		if (!token) throw new Error('Authorization token not provided');
-		const _post = await PostModel.findById(id);
-		if (!_post) throw new Error('Post not found');
-		await _post.deleteOne();
-		return res.status(200).send({
-			post: _post,
-			message: 'Post deleted successfully',
-		});
+		getUserInfo(req)
+			.then(async (payload) => {
+				const _user = await UserModel.findById(payload.id);
+				if (!_user) throw new Error('User not found');
+				const _post = await PostModel.findById(id);
+				if (!_post) throw new Error('Post not found');
+				const auth = _post.owner.equals(_user._id);
+				if (!auth) throw new Error('User is unauthorized to delete post');
+				await _post.deleteOne();
+				return res.status(200).send({
+					post: _post,
+					message: 'Post deleted successfully',
+				});
+			})
+			.catch((e) => {
+				return res.status(400).send({
+					post: null,
+					message: e,
+				});
+			});
 	} catch (err) {
-		return res.status(401).send({
+		return res.status(400).send({
 			post: null,
 			message: err.message,
 		});
