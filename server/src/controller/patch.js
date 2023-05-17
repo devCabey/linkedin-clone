e39363e18c86304b40d18b403e9c7d707c5af93c
Caleb Okyere <agyakojo4321@gmail.com>
2023-05-17 13:42:20 +0000

    patch controller modified

--- server/src/controller/patch.js
+++ server/src/controller/patch.js
@@ -3,34 +3,28 @@ import { UserModel } from '../model/index.js';
 
 export const updateUser = async (req, res) => {
 	const { lastName, firstName, about, profile } = req.body;
-	try {
-		const updater = {};
-		if (firstName) updater.firstName = firstName;
-		if (lastName) updater.lastName = lastName;
-		if (about) updater.about = about;
-		if (profile) updater.profile = profile;
-		authorize(req)
-			.then(async (payload) => {
-				const _user = await UserModel.findById(payload?.id);
-				if (!_user) throw new Error('User is not authorized');
-				await _user.updateOne({
-					$set: { ...updater },
-				});
-				return res.status(200).send({
-					user: _user,
-					message: 'User updated successfully',
-				});
-			})
-			.catch((e) => {
-				return res.status(400).send({
-					user: null,
-					message: e,
-				});
+
+	const updater = {};
+	if (firstName) updater.firstName = firstName;
+	if (lastName) updater.lastName = lastName;
+	if (about) updater.about = about;
+	if (profile) updater.profile = profile;
+	authorize(req)
+		.then(async (payload) => {
+			const _user = await UserModel.findById(payload?.id);
+			if (!_user) throw new Error('User is not authorized');
+			await _user.updateOne({
+				$set: { ...updater },
 			});
-	} catch (err) {
-		return res.status(400).send({
-			user: null,
-			message: err.message,
+			return res.status(200).send({
+				user: _user,
+				message: 'User updated successfully',
+			});
+		})
+		.catch((e) => {
+			return res.status(400).send({
+				user: null,
+				message: e?.message || e,
+			});
 		});
-	}
 };
