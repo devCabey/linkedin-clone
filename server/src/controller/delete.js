92ac694de9a5e486e690308c3f44d7276495509f
Caleb Okyere <agyakojo4321@gmail.com>
2023-05-20 22:02:50 +0000

    delete controller comment

--- server/src/controller/delete.js
+++ server/src/controller/delete.js
@@ -14,12 +14,16 @@ export const deletePost = async (req, res) => {
 		if (!id) throw new Error('Post ID not provided');
 		authorize(req)
 			.then(async (payload) => {
+				/** getting user using payload info from token */
 				const _user = await UserModel.findById(payload?.id);
 				if (!_user) throw new Error('User not found');
+				/** getting post info from post id passed through req.params */
 				const _post = await PostModel.findById(id);
 				if (!_post) throw new Error('Post not found');
+				/** checking if current user is the same as owner of post */
 				const auth = _post.owner.equals(_user._id);
 				if (!auth) throw new Error('User is unauthorized to delete post');
+				/** deleting post */
 				await _post.deleteOne();
 				return res.status(200).send({
 					post: _post,
@@ -52,19 +56,24 @@ export const deleteComment = async (req, res) => {
 	const { id, commentId } = req.params;
 	authorize(req)
 		.then(async (payload) => {
+			/** getting post from post id from req.params */
 			const _post = await PostModel.findById(id);
 			if (!_post) throw new Error('Post not found');
+			/** checking whether current user exist */
 			const _user = await UserModel.findById(payload.id);
 			if (!_user) throw new Error('User not found');
+			/** getting all comments */
 			const comments = [..._post.comments];
 			if (!comments.length) throw new Error('Comment is empty');
 			let data_id;
+			/** getting index of comment to delete */
 			const commentIndex = comments.findIndex((data) => {
 				data_id = data._id.toString();
 				return data_id == commentId;
 			});
-
+			/** checking whether comment exist */
 			if (commentIndex === -1) throw new Error('Comment not found');
+			/** removing comment and deleting */
 			comments.splice(commentIndex, 1);
 			await _post.updateOne({ $set: { comments } });
 			return res.status(200).send({
