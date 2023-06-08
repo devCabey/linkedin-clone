0f57ef505a3a123eb1a98e0f50464cd3847daad4
Caleb <agyakojo4321@gmail.com>
2023-06-08 13:26:15 +0000

    like post api done

--- client/src/api/get.js
+++ client/src/api/get.js
@@ -17,3 +17,12 @@ export const getPost = async (id) => {
 		throw new Error(e);
 	}
 };
+
+export const likePost = async (id) => {
+	try {
+		const response = await axios.get(`${process.env.SERVER_URL}/posts/${id}/like`);
+		return response;
+	} catch (e) {
+		throw new Error(e);
+	}
+};
