75706d87a86994430ff862ad6fc50a45555bfc6a
Caleb <agyakojo4321@gmail.com>
2023-06-08 13:19:51 +0000

    get posts done

---
+++ client/src/api/get.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const getPosts = async () => {
+	try {
+		const response = await axios.get(`${process.env.SERVER_URL}/posts`);
+		return response;
+	} catch (e) {
+		throw new Error(e);
+	}
+};
